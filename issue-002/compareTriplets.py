48d7b035cebb0de370403b0a98950f04d810522b
Antoh1 <antotiro@gmail.com>
2017-02-28 14:22:21 +0300

    Solution: hackerrank compareTriplets

--- issue-002/compareTriplets.py
+++ issue-002/compareTriplets.py
@@ -1,6 +1,6 @@
-a0,a1,a2 = input().strip().split(' ')
+a0,a1,a2 = raw_input().strip().split(' ')
 a0,a1,a2 = [int(a0),int(a1),int(a2)]
-b0,b1,b2 = input().strip().split(' ')
+b0,b1,b2 = raw_input().strip().split(' ')
 b0,b1,b2 = [int(b0),int(b1),int(b2)]
 
 def scores():
